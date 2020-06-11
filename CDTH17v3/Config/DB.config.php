9cdba684fd40db3c125df427a38039bb103324e1
luongdinhkhanhvinh <luongdkvinh@gmail.com>
2020-06-11 22:18:05 +0700

    FoodShopv3

---
+++ CDTH17v3/Config/DB.config.php
@@ -0,0 +1,8 @@
+<?php
+    define("HOSTDB", "remotemysql.com");
+    define("USERDB", "PsSqwxzcvi");
+    define("PASSDB", "ztrptQbPDb");
+    define("NAMEDB", "PsSqwxzcvi");
+    define("PORTDB", 3306);
+
+?>
